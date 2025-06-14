a64df5c9fc21b58c033e3845b97cb95a9db0fc34
Vaisakh Manu <133499771+ItzMeHyper@users.noreply.github.com>
2025-06-14 15:14:15 +0530

    Delete Vaisakh_Manu directory

--- Vaisakh_Manu/src/components/ui/chart.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils"
-
-export function Chart({ className }: { className?: string }) {
-  return (
-    <div className={cn("space-y-4", className)}>
-      {/* Chart implementation */}
-    </div>
-  )
-}
